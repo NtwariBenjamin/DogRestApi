450714ea921b3f1acc9e0154994f8ae918a3823f
Benjamin <benjamin.ntwali01@gmail.com>
2024-09-08 20:57:03 +0200

    Implementing Data Repositories by only using Entity and Repository with Including Microservices

--- src/main/java/com/benjamin/DogRestApi/DogRestApiApplication.java
+++ src/main/java/com/benjamin/DogRestApi/DogRestApiApplication.java
@@ -2,8 +2,10 @@ package com.benjamin.DogRestApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DogRestApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/benjamin/DogRestApi/repository/DogRepository.java
+++ src/main/java/com/benjamin/DogRestApi/repository/DogRepository.java
@@ -1,21 +1,19 @@
 package com.benjamin.DogRestApi.repository;
 
 import com.benjamin.DogRestApi.Entity.Dog;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-
-import java.lang.ref.SoftReference;
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
-public interface DogRepository extends CrudRepository<Dog,Integer> {
-    @Query("select d from Dog d where d.name=?1")
-    Dog findByName(String dogName);
-
-
-    @Query("select d.breed from Dog d")
-    List<String> findBreed();
-
-    @Query("select d from Dog d where d.breed=?1")
-    Dog findByBreed(String breed);
+@Repository
+public interface DogRepository extends JpaRepository<Dog,Integer> {
+//    @Query("select d from Dog d where d.name=?1")
+//    Dog findByName(String dogName);
+//
+//
+//    @Query("select d.breed from Dog d")
+//    List<String> findBreed();
+//
+//    @Query("select d from Dog d where d.breed=?1")
+//    Dog findByBreed(String breed);
 }
