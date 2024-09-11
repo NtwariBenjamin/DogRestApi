5c78fb75564ccf5e747e4071e7391a99582eb420
Benjamin <benjamin.ntwali01@gmail.com>
2024-09-11 10:19:09 +0200

    Added a spring security feature by Implementing Basic Authentication

--- src/main/java/com/benjamin/DogRestApi/web/DogController.java
+++ src/main/java/com/benjamin/DogRestApi/web/DogController.java
@@ -2,7 +2,7 @@ package com.benjamin.DogRestApi.web;
 
 import com.benjamin.DogRestApi.Entity.Dog;
 import com.benjamin.DogRestApi.service.DogService;
-import jakarta.persistence.criteria.CriteriaBuilder;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
